b940c4c5b285e1ee87c78354f2dcaeb93db6a241
songseongbin <ssbin0916@naver.com>
2024-06-19 02:46:50 +0900

    백준

---
+++ src/baekjoon/Q5597/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.Q5597;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] students = new int[31];
+
+        for (int i = 1; i < 29; i++) {
+            int studentNumber = scanner.nextInt();
+            students[studentNumber] = 1;
+        }
+
+        int missingCount = 0;
+        int[] missingStudents = new int[2];
+
+        for (int i = 1; i < students.length; i++) {
+            if (students[i] != 1) {
+                missingStudents[missingCount] = i;
+                missingCount++;
+            }
+        }
+
+        System.out.println(missingStudents[0]);
+        System.out.println(missingStudents[1]);
+    }
+}
