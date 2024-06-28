309bd0f8915d7e593a810e7714e831770f573c12
songseongbin <ssbin0916@naver.com>
2024-06-29 06:00:40 +0900

    백준

---
+++ src/baekjoon/Q2742/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.Q2742;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for (int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
