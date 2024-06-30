543769dcb628543e73709c2a55281f53d0956d8e
songseongbin <ssbin0916@naver.com>
2024-07-01 02:17:36 +0900

    백준

---
+++ src/baekjoon/Q4101/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.Q4101;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+
+            if (A == 0 && B == 0) {
+                break;
+            }
+
+            if (A > B) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
