e596a55c8e110ab952be566707d6866046e51c2d
songseongbin <ssbin0916@naver.com>
2024-07-14 18:53:53 +0900

    백준

---
+++ src/baekjoon/page25/Q25372/Main.java
@@ -0,0 +1,24 @@
+package baekjoon.page25.Q25372;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            if (str.length() >= 6 && str.length() <= 9) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
