fabbd5d5f699557f97e46c51980ee04cf013a5bc
songseongbin <ssbin0916@naver.com>
2024-07-19 13:00:55 +0900

    백준

---
+++ src/baekjoon/page13/Q13752/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.page13.Q13752;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int k = Integer.parseInt(br.readLine());
+            for (int j = 0; j < k; j++) {
+                System.out.print("=");
+            }
+            System.out.println();
+        }
+
+        br.close();
+    }
+}
