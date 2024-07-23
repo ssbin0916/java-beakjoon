5980f37784627339589328990a1a832a5897648c
songseongbin <ssbin0916@naver.com>
2024-07-23 10:03:47 +0900

    백준

---
+++ src/baekjoon/page2/Q2460/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.page2.Q2460;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[10];
+        int result = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            result = result + (b - a);
+            arr[i] = result;
+        }
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
