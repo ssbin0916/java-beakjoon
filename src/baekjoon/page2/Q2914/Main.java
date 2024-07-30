eaef2a750af16c2dad874509f426073f461e642f
songseongbin <ssbin0916@naver.com>
2024-07-30 22:53:47 +0900

    백준

--- src/baekjoon/page2/Q2914/Main.java
+++ src/baekjoon/page2/Q2914/Main.java
@@ -1,12 +1,16 @@
 package baekjoon.page2.Q2914;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
     }
 }
