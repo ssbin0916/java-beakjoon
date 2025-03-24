a76c542ec19a875d4bb52f081905c4fccc8e7a0d
songseongbin <ssbin0916@naver.com>
2025-03-24 19:24:09 +0900

    백준

---
+++ src/baekjoon/page10/Q10845/Main.java
@@ -0,0 +1,88 @@
+package baekjoon.page10.Q10845;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[] queue;
+    public static int front = 0;
+    public static int rear = 0;
+    public static int size = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int number = Integer.parseInt(br.readLine());
+        queue = new int[number];
+
+        while (number-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            if (command.equals("push")) {
+                push(Integer.parseInt(st.nextToken()));
+            }
+            if (command.equals("pop")) {
+                sb.append(pop()).append("\n");
+            }
+            if (command.equals("size")) {
+                sb.append(size()).append("\n");
+            }
+            if (command.equals("empty")) {
+                sb.append(empty()).append("\n");
+            }
+            if (command.equals("front")) {
+                sb.append(front()).append("\n");
+            }
+            if (command.equals("back")) {
+                sb.append(back()).append("\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void push(int value) {
+        queue[rear++] = value;
+        size++;
+    }
+
+    public static int pop() {
+        if (size == 0) {
+            return -1;
+        }
+        size--;
+        return queue[front++];
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if (size == 0) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static int front() {
+        if (size == 0) {
+            return -1;
+        }
+        return queue[front];
+    }
+
+    public static int back() {
+        if (size == 0) {
+            return -1;
+        }
+        return queue[rear - 1];
+    }
+}
